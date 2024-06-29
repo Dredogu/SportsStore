a2e863c79012294a17051e89c0bac19a4cff9df3
Doğukan Aksoy <dgnkaksy06@gmail.com>
2024-06-29 16:50:10 +0300

    Add project files.

---
+++ SportsStore/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+using System.Diagnostics;
+
+namespace SportsStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IStoreRepository repository;
+
+        public HomeController(IStoreRepository repo)
+        {
+            repository = repo;
+        }
+
+        public IActionResult Index()
+        {
+            return View(repository.Products);
+        }
+    }
+}
